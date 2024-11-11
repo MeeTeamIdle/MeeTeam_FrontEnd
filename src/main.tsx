dcbbeb99cedc7fac188d518f1810cd02771d448c
prgmr99 <johnyeom24@naver.com>
2024-11-11 22:33:08 +0900

    fix: #277 구인글 상세 url 수정 및 PrivateRouter 처리

--- src/main.tsx
+++ src/main.tsx
@@ -69,6 +69,10 @@ const router = createBrowserRouter([
 						path: '',
 						element: <RecruitPage />,
 					},
+					{
+						path: 'recruitment/postings/:id',
+						element: <RecruitDetailPage />,
+					},
 				],
 			},
 			{
