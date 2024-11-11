8e176ef4194b12b398e01a04b70256538e23c8bf
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-11-11 19:20:45 +0900

    feat: #275 반응형에 따른 LinkDetails, ProfileImage 컴포넌트 스타일링 변경

--- src/components/link/details/LinkDetails.tsx
+++ src/components/link/details/LinkDetails.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import S from './LinkDetails.styled';
 import { Link } from '../../../types';
 import { Clip } from '../../../assets';
+import { useResponsiveWeb } from '../../../hooks';
 
 const LinkDetails = ({ url, description }: Link) => {
+	// 반응형
+	const [isMobilePort, isTabletPort] = useResponsiveWeb();
+
 	return (
-		<S.LinkDetailsLayout>
+		<S.LinkDetailsLayout $isTabletPort={isTabletPort} $isMobilePort={isMobilePort}>
 			<h4>{description}</h4>
-			<img src={Clip} alt='링크 아이콘' />
-			<a href={url} target='_blank' title={description} rel='noreferrer noopener'>
-				{url}
-			</a>
+			<div>
+				<img src={Clip} alt='링크 아이콘' />
+				<a href={url} target='_blank' title={description} rel='noreferrer noopener'>
+					{url}
+				</a>
+			</div>
 		</S.LinkDetailsLayout>
 	);
 };
