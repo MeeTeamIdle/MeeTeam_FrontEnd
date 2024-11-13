f793c130c3e8b1f7c7e538c0653c543f9a21822c
prgmr99 <johnyeom24@naver.com>
2024-11-13 17:37:20 +0900

    feat: #290 교내 버튼 예외처리

--- src/components/recruit/recruitBoard/toggle/CampusToggle.tsx
+++ src/components/recruit/recruitBoard/toggle/CampusToggle.tsx
@@ -3,34 +3,20 @@ import S from './CampusToggle.styled';
 import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 import { recruitFilterState, recruitFilterStateAuth } from '../../../../atom';
+import { useLogin } from '../../../../hooks';
 
 const CampusToggle = () => {
 	const navigate = useNavigate();
+	const { isLogin } = useLogin();
 	const setFilterState = useSetRecoilState(recruitFilterState);
 	const setFilterStateAuth = useSetRecoilState(recruitFilterStateAuth);
 
 	const [isCampus, setIsCampus] = useState<boolean>(false);
 
-	const handleCampusInClick = () => {
-		navigate('/campus');
-		setFilterState({
-			scope: 1,
-			category: null,
-			field: null,
-			skill: [],
-			role: [],
-			tag: [],
-			keyword: '',
-			course: null,
-			professor: null,
-		});
-		setIsCampus(true);
-	};
-
 	const handleCampusOutClick = () => {
 		navigate('/');
 		setFilterStateAuth({
-			scope: 2,
+			scope: 1,
 			category: null,
 			field: null,
 			skill: [],
@@ -43,19 +29,40 @@ const CampusToggle = () => {
 		setIsCampus(false);
 	};
 
+	const handleCampusInClick = () => {
+		if (!isLogin) {
+			navigate('/signin');
+			return;
+		}
+
+		navigate('/campus');
+		setFilterState({
+			scope: 2,
+			category: null,
+			field: null,
+			skill: [],
+			role: [],
+			tag: [],
+			keyword: '',
+			course: null,
+			professor: null,
+		});
+		setIsCampus(true);
+	};
+
 	return (
 		<S.CampusToggle>
-			<article
-				className={`wrapper ${isCampus ? 'selected' : 'notSelected'}`}
-				onClick={handleCampusInClick}
-			>
-				<span className='h3'>교내</span>
-			</article>
 			<article
 				className={`wrapper ${isCampus ? 'notSelected' : 'selected'}`}
 				onClick={handleCampusOutClick}
 			>
 				<span className='h3'>교외</span>
+			</article>
+			<article
+				className={`wrapper ${isCampus ? 'selected' : 'notSelected'}`}
+				onClick={handleCampusInClick}
+			>
+				<span className='h3'>교내</span>
 			</article>
 		</S.CampusToggle>
 	);
